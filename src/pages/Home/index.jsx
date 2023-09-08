7c9fee03c288fcae86e38a25a2da9216d87c587f
unknown <abu040810@gmail.com>
2023-09-08 16:36:48 +0500

    o

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,16 @@
 
 import Intro from "../../components/Intro";
 import SearchPanel from "../../components/UI/SearchPanel/SearchPanel";
+import Category from "../../components/Category/Category";
 import "./style.scss"
 
 const index = () => {
     return (
-        <div>
+        <div className="mainsection">
             <Intro/>
             <SearchPanel/>
+            <Category/>
+            Home
         </div>
     );
 };
--- src/pages/Setting/index.jsx
+++ src/pages/Setting/index.jsx
@@ -1,12 +1,25 @@
 
-
-import { Outlet } from "react-router-dom";
-
+import { useEffect } from "react";
+import { Outlet, useNavigate, Link } from "react-router-dom";
+import "./style.scss";
+import SettingHeader from "./SettingHeader";
+import { Button } from "antd";
 const index = () => {
+    const navigate = useNavigate();
+    useEffect(() => {
+        navigate("/settings/my-account")
+    }, [])
     return (
         <div>
-            <h1>Settings</h1>
+            <SettingHeader />
             <Outlet />
+
+            <div className="container">
+                <Link to="/">
+                    <Button className="mt-12 mb-5">Bosh sahifa</Button>
+                </Link>
+
+            </div>
         </div>
     );
 };
